7b3880d02a02455895313e204c4bd5018d3247c0
Josselyn-Ayo <josselyn.ayoharo@epn.edu.ec>
2026-01-28 09:52:11 -0500

    Update Azure server details in db.php

--- db.php
+++ db.php
@@ -8,10 +8,10 @@ header("Content-Type: application/json; charset=UTF-8");
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') { exit; }
 
 // Datos de Azure
-$azServer = "papeleria0.database.windows.net";
+$azServer = "papeleria2.database.windows.net";
 $azInfo = [
     "Database" => "papeleria",
-    "UID" => "papeleriaAdmin",
+    "UID" => "papeleriaA",
     "PWD" => "dios1234%", 
     "CharacterSet" => "UTF-8",
     "Encrypt" => 1,
@@ -42,4 +42,5 @@ if (!$conn) {
     ]));
 }
 // Al terminar este archivo, $conn ya está listo para usarse.
+
 ?>
